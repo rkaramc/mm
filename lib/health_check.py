e519eda87f8648eb8868fdcb9f278c249b01c6a7
Joe Ferraro <jf31782@gmail.com>
2013-09-26 17:39:30 +0200

    add visualforce to health check

--- lib/health_check.py
+++ lib/health_check.py
@@ -15,6 +15,13 @@ query_for_loop_pattern          = re.compile(r"(for|while) *?\(.*?\).*?{.*?\[ *S
 soql_query_pattern              = re.compile(r"\[ *SELECT\b.*?\]", re.IGNORECASE|re.MULTILINE|re.DOTALL)
 db_query_pattern                = re.compile(r"Database.query\(.*?\) *;", re.IGNORECASE|re.MULTILINE|re.DOTALL)
 
+where_seed      = 'where'
+not_like_seed   = 'not like'
+where_pattern       = re.compile(r'\b%s\b' % where_seed, re.I)
+not_like_pattern    = re.compile(r'\b%s\b' % not_like_seed, re.I)
+
+js_refresh_pattern = re.compile(r"(<meta[\s*]http-equiv=\"refresh\"[\s*]content=\"[0-9]+\"|window.location.reload|history.go\(0\))", re.IGNORECASE|re.MULTILINE|re.DOTALL)
+
 class HealthCheck(object):
 
     def __init__(self, project_location):
@@ -26,7 +33,8 @@ class HealthCheck(object):
         self.soql_negative_operators    = None
         self.soql_no_where_clause       = None
         self.action_poller              = None
-        self.multiple_form              = None
+        self.javascript_refresh         = None
+        self.meta_refresh               = None
         self.see_all_data               = None
         self.without_sharing_count      = 0
         self.dml_for_loop_count         = 0
@@ -35,11 +43,14 @@ class HealthCheck(object):
         self.negative_soql_count        = 0
         self.no_where_clause_count      = 0
         self.action_poller_count        = 0
-        self.multiple_form_count        = 0
+        self.javascript_refresh_count   = 0
+        self.meta_refresh_count         = 0
         self.see_all_data_count         = 0
+        self.hardcoded_link_count       = 0
         self.apex_files_to_check        = []
         self.vf_files_to_check          = []
-        self.parser_results             = {}
+        self.apex_parser_results        = {}
+        self.vf_parser_results          = {}
         self.apex_result                = {}
         self.vf_result                  = {}
         self.result                     = {
@@ -77,18 +88,23 @@ class HealthCheck(object):
             }
             ,
             "visualforce_statistics" : {
-                "action_poller" : {
-                    "label"     : "Visualforce Pages using actionpoller",
+                "action_poller"         : {
+                    "label"     : "Visualforce Pages using actionpoller with interval <= 60",
                     "count"     : 0,
                     "results"   : []
                 },
-                "multiple_form" : {
-                    "label"     : "Visualforce Pages with multiple forms",
+                "javascript_refresh"    : {
+                    "label"     : "Visualforce Pages using javascript refresh",
                     "count"     : 0,
                     "results"   : []
                 },
-                "hardcoded_url" : {
-                    "label"     : "Visualforce Pages with hardcoded URLs",
+                "meta_refresh"    : {
+                    "label"     : "Visualforce Pages using meta refresh",
+                    "count"     : 0,
+                    "results"   : []
+                },
+                "hardcoded_url"         : {
+                    "label"     : "Visualforce Pages using outputLinks with hardcoded URLs",
                     "count"     : 0,
                     "results"   : []
                 }
@@ -105,24 +121,131 @@ class HealthCheck(object):
                 elif ext in vf_extensions_to_check:
                     self.vf_files_to_check.append(full_file_path)
 
-        parser_threads = []
+        apex_parser_threads = []
+        vf_parser_threads   = []
 
-        file_chunks = list(mm_util.grouper(8, self.apex_files_to_check))
-        for files in file_chunks:                    
-            thread = ApexParser(files)
-            parser_threads.append(thread)
-            thread.start()
-            
-        for thread in parser_threads:
+        apex_file_chunks    = list(mm_util.grouper(8, self.apex_files_to_check))
+        vf_file_chunks      = list(mm_util.grouper(8, self.vf_files_to_check))
+
+        for files in apex_file_chunks:                    
+            apex_parser_thread = ApexParser(files)
+            apex_parser_threads.append(apex_parser_thread)
+            apex_parser_thread.start()
+
+        for files in vf_file_chunks:                    
+            vf_parser_thread = VfParser(files)
+            vf_parser_threads.append(vf_parser_thread)
+            vf_parser_thread.start()
+
+        for thread in apex_parser_threads:
             thread.join()
             if thread.complete:
-                self.parser_results.update(thread.result)
+                self.apex_parser_results.update(thread.result)
+
+        for thread in vf_parser_threads:
+            thread.join()
+            if thread.complete:
+                self.vf_parser_results.update(thread.result)
         
         #pp = pprint.PrettyPrinter(indent=2)
         #pp.pprint(self.parser_results)
 
+        for file_name in self.vf_files_to_check:
+            parser_result = self.vf_parser_results[file_name]
+
+            base_name = os.path.basename(file_name)
+            self.vf_result[base_name] = {}
+            file_body = mm_util.get_file_as_string(file_name)
+
+            ### ACTION POLLERS
+            if "actionPollers" not in parser_result:
+                #print file_name
+                continue
+
+            action_pollers = parser_result["actionPollers"]
+            action_poller_matches = []
+            if len(action_pollers) > 0:
+                for p in action_pollers:
+                     
+                    line_contents = ""
+                    for lnum in range(p["location"]["row"], p["location"]["row"]+1):
+                        line_contents += print_file_line(file_name, lnum)
+                    
+                    p["lineNumber"]       = p["location"]["row"]
+                    p["line_contents"]    = line_contents
+
+                    action_poller_matches.append(p)  
+
+
+                self.result["visualforce_statistics"]["action_poller"]["results"].append(
+                    {
+                        "file_name" : base_name,
+                        "flagged"   : len(action_poller_matches) > 0,
+                        "matches"   : action_poller_matches   
+                    }
+                )
+                self.action_poller_count += len(action_poller_matches)
+
+            ### HARDCODED URLS
+            output_links = parser_result["outputLinks"]
+            link_matches = []
+            if len(output_links) > 0:
+                for p in output_links:
+                     
+                    line_contents = ""
+                    for lnum in range(p["location"]["row"], p["location"]["row"]+1):
+                        line_contents += print_file_line(file_name, lnum)
+                    
+                    p["lineNumber"]       = p["location"]["row"]
+                    p["line_contents"]    = line_contents
+
+                    link_matches.append(p)  
+
+
+                self.result["visualforce_statistics"]["hardcoded_url"]["results"].append(
+                    {
+                        "file_name" : base_name,
+                        "flagged"   : len(link_matches) > 0,
+                        "matches"   : link_matches   
+                    }
+                )
+                self.hardcoded_link_count += len(link_matches)
+
+            ## REFRESHERS
+            refreshers = re.finditer(js_refresh_pattern, file_body)          
+            js_matches      = []
+            meta_matches    = []
+            for match in refreshers:
+                if match != None and "meta" in match.group(0):
+                    match_string = match.group(0).replace("<", "")    
+                    meta_matches.append(match_string)
+                else:
+                    match_string = match.group(0)
+                    js_matches.append(match_string)
+            if len(js_matches) > 0:
+                self.result["visualforce_statistics"]["javascript_refresh"]["results"].append(
+                    {
+                        "file_name" : base_name,
+                        "flagged"   : len(js_matches) > 0,
+                        "matches"   : js_matches   
+                    }
+                )
+                self.javascript_refresh_count += len(js_matches)
+
+            if len(meta_matches) > 0:
+                self.result["visualforce_statistics"]["meta_refresh"]["results"].append(
+                    {
+                        "file_name" : base_name,
+                        "flagged"   : len(meta_matches) > 0,
+                        "matches"   : meta_matches   
+                    }
+                )
+                self.meta_refresh_count += len(meta_matches)
+
+
+
         for file_name in self.apex_files_to_check:
-            parser_result = self.parser_results[file_name]
+            parser_result = self.apex_parser_results[file_name]
 
             base_name = os.path.basename(file_name)
             self.apex_result[base_name] = {}
@@ -205,9 +328,11 @@ class HealthCheck(object):
             for query in queries:
                 line_number = query["lineNumber"]
                 lower_query = query["statement"].lower()
-                if ' where ' not in lower_query:
+                #if ' where ' not in lower_query:
+                if where_pattern.search(lower_query) == None:
                     no_where_clause_matches.append(query)
-                if ' not like ' in lower_query or '!=' in lower_query:
+                #if ' not like ' in lower_query or '!=' in lower_query:
+                if not_like_pattern.search(lower_query) != None or "!=" in lower_query:
                     negative_operator_matches.append(query)
 
             if len(no_where_clause_matches) > 0:
@@ -285,9 +410,10 @@ class HealthCheck(object):
         self.result["apex_statistics"]["soql_no_where_clause"]["count"]     = self.no_where_clause_count
         self.result["apex_statistics"]["see_all_data_annotations"]["count"] = self.see_all_data_count
 
-        self.result["visualforce_statistics"]["action_poller"]["count"]     = self.action_poller_count
-        self.result["visualforce_statistics"]["multiple_form"]["count"]     = self.multiple_form_count
-        
+        self.result["visualforce_statistics"]["action_poller"]["count"]      = self.action_poller_count
+        self.result["visualforce_statistics"]["javascript_refresh"]["count"] = self.javascript_refresh_count
+        self.result["visualforce_statistics"]["meta_refresh"]["count"]       = self.meta_refresh_count
+        self.result["visualforce_statistics"]["hardcoded_url"]["count"]      = self.hardcoded_link_count
         return self.result
 
 
@@ -312,6 +438,41 @@ class ApexParser(threading.Thread):
         for f in self.files:
             parser_command = 'java -jar "{0}" "{1}"'.format(
                 os.path.join(config.base_path,"bin","apex-parser.jar"),
+                f)
+
+            result_string = ""
+            p = subprocess.Popen(parser_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            if p.stdout is not None : 
+                for line in p.stdout.readlines():
+                    result_string += line
+            elif p.stderr is not None :
+                print "****ERROR****"
+                for line in p.stderr.readlines():
+                    print line
+
+            try:    
+                self.result[f] = json.loads(result_string)
+            except:
+                #print "error on: ",self.file_location
+                result = {
+                    "success"           : False,
+                    "file_location"     : f
+                }
+                self.result[f] = result
+        self.complete = True
+
+class VfParser(threading.Thread):
+
+    def __init__(self, files):
+        self.files          = files
+        self.result         = {}
+        self.complete       = False
+        threading.Thread.__init__(self)
+
+    def run(self):
+        for f in self.files:
+            parser_command = 'java -jar "{0}" "{1}"'.format(
+                os.path.join(config.base_path,"bin","vf-parser.jar"),
                 f)
 
             result_string = ""
--- test/project/health_check_runner.py
+++ test/project/health_check_runner.py
@@ -6,8 +6,9 @@ sys.path.append('../../')
 
 from lib.health_check import HealthCheck
 #check = HealthCheck("/Users/josephferraro/Development/Github/mm/test/test_workspace/unit test health check project")
-check = HealthCheck("/Users/josephferraro/Development/st/rc9")
+#check = HealthCheck("/Users/josephferraro/Development/st/rc9")
 #check = HealthCheck("/Users/josephferraro/Development/st/dreamforce")
+check = HealthCheck("/Users/josephferraro/Development/st/dreamforce (sbob)")
 
 result = check.run()
 pp = pprint.PrettyPrinter(indent=2)
