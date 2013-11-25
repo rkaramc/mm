f85fd97d30136cc3160dae87410da452afbe5621
Joe Ferraro <jf31782@gmail.com>
2013-11-25 21:23:06 +0100

    resolves issue where local session is in conflict with preferences which causes messiness with metadata api parsing
    refs joeferraro/MavensMate#140

--- lib/mm_project.py
+++ lib/mm_project.py
@@ -62,20 +62,13 @@ class MavensMateProject(object):
 
             if self.ui == False and self.defer_connection == False:
                 needs_session_override = False
-                if int(float(mm_util.SFDC_API_VERSION)) >= 29 and self.sfdc_session != None:
-                    server_url = self.sfdc_session['server_url']
-                    if '28.0' in server_url:
-                        needs_session_override = True
-                    elif '27.0' in server_url:
-                        needs_session_override = True
-                    elif '26.0' in server_url:
-                        needs_session_override = True
-                    elif '25.0' in server_url:
-                        needs_session_override = True
-                    elif '24.0' in server_url:
+                if self.sfdc_session != None and 'endpoint' in self.sfdc_session:
+                    endpoint = self.sfdc_session['endpoint']
+                    api_version_preference = int(float(mm_util.SFDC_API_VERSION))
+                    if int(float(endpoint.split("/")[-1])) != api_version_preference:
                         needs_session_override = True
 
-                self.sfdc_client        = MavensMateClient(credentials=self.get_creds(),override_session=needs_session_override)
+                self.sfdc_client = MavensMateClient(credentials=self.get_creds(),override_session=needs_session_override)
 
                 if self.sfdc_session != None and 'sid' in self.sfdc_session and self.sfdc_client != None and (self.sfdc_session['sid'] != self.sfdc_client.sid): 
                     config.logger.debug('storing updated session information locally')
