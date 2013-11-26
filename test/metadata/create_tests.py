0d1fc43b3089a8c2970599b8768465d009f214f2
Joe Ferraro <jf31782@gmail.com>
2013-11-26 17:16:57 +0100

    better metadata tests

--- test/metadata/create_tests.py
+++ test/metadata/create_tests.py
@@ -15,13 +15,23 @@ import lib.mm_client as sfdc
 
 base_test_directory = os.path.dirname(os.path.dirname(__file__))
 
-class ToolingTests(MavensMateTest):
+class MetadataTests(MavensMateTest):
     
-    def test_01_new_debug_log(self): 
+    def test_01_new_apex_class(self): 
         test_helper.create_project("unit test metadata project")
         commandOut = self.redirectStdOut()
         stdin = {
-            "project_name"      : "unit test metadata project",
+            "github_template": {
+                "author": "MavensMate", 
+                "description": "The default template for an Apex Class", 
+                "name": "Default", 
+                "file_name": "ApexClass.cls"
+            }, 
+            "apex_trigger_object_api_name": None, 
+            "apex_class_type": None, 
+            "api_name": "unittestapexclass", 
+            "project_name": "unit test metadata project", 
+            "metadata_type": "ApexClass"
         }
         mm_util.get_request_payload = mock.Mock(return_value=stdin)
         sys.argv = ['mm.py', '-o', 'new_metadata']
@@ -29,9 +39,26 @@ class ToolingTests(MavensMateTest):
         mm_response = commandOut.getvalue()
         sys.stdout = self.saved_stdout
         print mm_response
-        #mm_json_response = util.parse_mm_response(mm_response)
-        #self.assertTrue(mm_json_response['success'] == True)
-        #self.assertTrue('id' in mm_json_response and len(mm_json_response['id']) is 18)
+        mm_json_response = util.parse_mm_response(mm_response)
+        self.assertTrue(mm_json_response['success'] == True)
+        self.assertTrue('id' in mm_json_response and len(mm_json_response['id']) is 18)
+
+    def test_02_delete_apex_class(self): 
+        commandOut = self.redirectStdOut()
+        client_settings = mm_util.parse_json_from_file(os.path.join(base_test_directory, "user_client_settings.json"))
+        stdin = {
+            "files": [os.path.join(client_settings["mm_workspace"],"unit test metadata project","src","classes","unittestapexclass.cls")], 
+            "project_name": "unit test metadata project"
+        }
+
+        mm_util.get_request_payload = mock.Mock(return_value=stdin)
+        sys.argv = ['mm.py', '-o', 'delete']
+        mm.main()
+        mm_response = commandOut.getvalue()
+        sys.stdout = self.saved_stdout
+        print mm_response
+        mm_json_response = util.parse_mm_response(mm_response)
+        self.assertTrue(mm_json_response['success'] == True)
 
     # def test_02_new_quicklog(self): 
     #     commandOut = self.redirectStdOut()
