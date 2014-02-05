73180352f1cce8dd3e2532321ec0c905aa6f824d
Joe Ferraro <jf31782@gmail.com>
2014-02-05 19:57:55 +0100

    resolved signing issues so this re goes away

--- bin/osx/mm/lib/basecommand.py
+++
@@ -1,7 +0,0 @@
-class Command(object):
-    def __init__(self, *args, **kwargs):
-        self.params = kwargs.get('params', {})
-        self.args = kwargs.get('args', None)
-
-    def execute(self):
-        raise NotImplementedError()
